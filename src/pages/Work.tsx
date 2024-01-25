4287d843203ee7cc601423b34fa2afe7addd8c8b
JJoanka <joanna.jurasz21@gmail.com>
2024-01-25 12:06:39 +0100

    Updates to fullstack

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -12,47 +12,6 @@ const Work = () => {
       <Menu></Menu>
       <Transitions>
         <div className={styles.container}>
-          <h1 className={styles.header}>Technologies I work with</h1>
-          <div className={styles.logos}>
-            <div className={styles.line}>
-              <TechIcon icon="/assets/techLogos/html.svg" tooltipText="HTML" />
-              <TechIcon icon="/assets/techLogos/css.svg" tooltipText="CSS" />
-              <TechIcon
-                icon="/assets/techLogos/js.svg"
-                tooltipText="JavaScript"
-              />
-              <TechIcon
-                icon="/assets/techLogos/ts.svg"
-                tooltipText="TypeScript"
-              />
-              <TechIcon
-                icon="/assets/techLogos/react.svg"
-                tooltipText="React"
-              />
-            </div>
-            <div className={styles.line}>
-              <TechIcon icon="/assets/techLogos/git.svg" tooltipText="Git" />
-              <TechIcon icon="/assets/techLogos/sass.svg" tooltipText="SASS" />
-              <TechIcon icon="/assets/techLogos/npm.svg" tooltipText="NPM" />
-              <TechIcon icon="/assets/techLogos/yarn.svg" tooltipText="YARN" />
-            </div>
-            <div className={styles.line}>
-              <TechIcon
-                icon="/assets/techLogos/linux.svg"
-                tooltipText="Linux"
-              />
-              <TechIcon
-                icon="/assets/techLogos/windows.svg"
-                tooltipText="Windows"
-              />
-              <TechIcon
-                icon="/assets/techLogos/vsc.svg"
-                tooltipText="Visual Studio Code"
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles.container}>
           <h1 className={styles.header}>Work history</h1>
           <div className={styles.project}>
             <div className={styles.item1}>
@@ -82,6 +41,31 @@ const Work = () => {
                 something is added or removed from the origin file. For this
                 job, I have chosen technologies like MongoDB, Express.js, React
                 and Vite
+              </Box>
+            </div>
+          </div>
+          <h1 className={styles.header}>Internships</h1>
+          <div className={styles.project}>
+            <div className={styles.item1}>
+              <Box header={"Application for estate agents"}>
+                <div className={styles.date}>
+                  <i>(2023.04 - 2023.11)</i>
+                </div>
+                <br />
+                &emsp;I was in a group of seniors and a few juniors working on a
+                project to develop a web application for estate agents. Its
+                features included a&nbsp;list of clients who can be matched with
+                estates in&nbsp;the database based on their preferences, as well
+                as a meeting manager. My responsibilities during creating the
+                application involved the&nbsp;implementation of a form for
+                adding new meetings. This project allowed me to learn and
+                practice group working using technologies like Github and Jira.
+              </Box>
+            </div>
+            <div className={styles.item2}>
+              <Box header={"Technologies"}>
+                React, Typescript, Nextjs, Styled&nbsp;components, Graphql,
+                Nest, Prisma
               </Box>
             </div>
           </div>
@@ -157,31 +141,6 @@ const Work = () => {
           <EndOfSection></EndOfSection>
           <div className={styles.project}>
             <div className={styles.item1}>
-              <Box header={"Application for estate agents"}>
-                <div className={styles.date}>
-                  <i>(2023.04 - 2023.11)</i>
-                </div>
-                <br />
-                &emsp;I was in a group of seniors and a few juniors working on a
-                project to develop a web application for estate agents. Its
-                features included a&nbsp;list of clients who can be matched with
-                estates in&nbsp;the database based on their preferences, as well
-                as a meeting manager. My responsibilities during creating the
-                application involved the&nbsp;implementation of a form for
-                adding new meetings. This project allowed me to learn and
-                practice group working using technologies like Github and Jira.
-              </Box>
-            </div>
-            <div className={styles.item2}>
-              <Box header={"Technologies"}>
-                React, Typescript, Nextjs, Styled&nbsp;components, Graphql,
-                Nest, Prisma
-              </Box>
-            </div>
-          </div>
-          <EndOfSection></EndOfSection>
-          <div className={styles.project}>
-            <div className={styles.item1}>
               <Box
                 header={
                   "Creating a hemoglobin measuring device using Arduino and 3D printing technology"
@@ -192,7 +151,7 @@ const Work = () => {
                 </div>
                 <br />
                 &emsp;This project was a part of my master's thesis. I was
-                created small and mobile POCT (Point of Care Testing) device,
+                creating small and mobile POCT (Point of Care Testing) device,
                 allowing for hemoglobin concentration measurement in capillary
                 blood. <br /> <br />
                 My work included: <br />- designing the optical system and all
@@ -221,6 +180,47 @@ const Work = () => {
             </div>
           </div>
           <EndOfSection></EndOfSection>
+        </div>
+        <div className={styles.container}>
+          <h1 className={styles.header}>Technologies I work with</h1>
+          <div className={styles.logos}>
+            <div className={styles.line}>
+              <TechIcon icon="/assets/techLogos/html.svg" tooltipText="HTML" />
+              <TechIcon icon="/assets/techLogos/css.svg" tooltipText="CSS" />
+              <TechIcon
+                icon="/assets/techLogos/js.svg"
+                tooltipText="JavaScript"
+              />
+              <TechIcon
+                icon="/assets/techLogos/ts.svg"
+                tooltipText="TypeScript"
+              />
+              <TechIcon
+                icon="/assets/techLogos/react.svg"
+                tooltipText="React"
+              />
+            </div>
+            <div className={styles.line}>
+              <TechIcon icon="/assets/techLogos/git.svg" tooltipText="Git" />
+              <TechIcon icon="/assets/techLogos/sass.svg" tooltipText="SASS" />
+              <TechIcon icon="/assets/techLogos/npm.svg" tooltipText="NPM" />
+              <TechIcon icon="/assets/techLogos/yarn.svg" tooltipText="YARN" />
+            </div>
+            <div className={styles.line}>
+              <TechIcon
+                icon="/assets/techLogos/linux.svg"
+                tooltipText="Linux"
+              />
+              <TechIcon
+                icon="/assets/techLogos/windows.svg"
+                tooltipText="Windows"
+              />
+              <TechIcon
+                icon="/assets/techLogos/vsc.svg"
+                tooltipText="Visual Studio Code"
+              />
+            </div>
+          </div>
         </div>
       </Transitions>
     </Card>
