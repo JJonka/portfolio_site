2a4a3ba3fdc53ce0f9861d16ddf3e0db7fb0d652
JJoanka <joanna.jurasz21@gmail.com>
2024-02-22 22:38:59 +0100

    Adds fixes

--- src/components/Copyright/Copyright.test.tsx
+++ src/components/Copyright/Copyright.test.tsx
@@ -6,7 +6,7 @@ import Copyright from "./Copyright";
 describe("Copyright", () => {
   it("should render the component properly", () => {
     // ARRANGE && ACT
-    const { getByText } = render(<Copyright></Copyright>);
+    const { getByText } = render(<Copyright page=""></Copyright>);
 
     // ASSERT
     expect(getByText("© 2024 Joanna Jurasz")).toBeInTheDocument();
